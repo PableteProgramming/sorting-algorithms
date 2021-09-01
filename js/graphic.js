f470e95b5b09d63c1857799e4a2f69c22c832145
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2021-09-01 11:38:20 +0200

    Adding graphic

---
+++ js/graphic.js
@@ -0,0 +1,32 @@
+var numbers= [];
+var numbersNum=150;
+
+window.onresize= Draw;
+
+
+function Generate(){
+	while(numbers.length < numbersNum){
+		let value=Math.floor(Math.random()* (numbersNum+1));
+		if (numbers.indexOf(value) == -1 && value != 0) {
+            numbers.push(value);
+        }
+	}
+	Draw();
+}
+
+function Draw(){
+	// empty the container
+	document.getElementById("Graphic-container").innerHTML="";
+
+	//container size
+	let contSize= window.innerWidth * 0.8;
+
+	let barWidth= contSize / numbersNum;
+
+	console.log(barWidth);
+	
+	for(let i=0; i< numbers.length; i++){
+		let line= '<div class="Barline" style="height: '+numbers[i]/2.4+'vh; width: '+barWidth+'px;"></div>';
+			document.getElementById("Graphic-container").innerHTML += line;
+	}
+}
