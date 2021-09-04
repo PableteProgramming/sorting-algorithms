1add424fb4d01158ce0bf7c6e817dd908ad4359b
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2021-09-04 19:15:27 +0200

    Adding funtions

--- js/graphic.js
+++ js/graphic.js
@@ -1,17 +1,32 @@
 var numbers= [];
+var numbersDesc= [];
 var numbersNum=150;
+var maxnumValue=200;
 var sortingalg="Insertion";
 
 window.onresize= Draw;
 window.onload= Generate;
 
+function occurrences(arr,string){
+	let r=0;
+	for(let i=0; i<arr.length; i++){
+		if(arr[i]==string){
+			r+=1;
+		}
+	}
+	return r;
+}
+
 function Generate(){
+	numbersDesc= [];
 	numbers= [];
 	while(numbers.length < numbersNum){
-		let value=Math.floor(Math.random()* (numbersNum+1));
-		if (numbers.indexOf(value) == -1 && value != 0) {
+		let value=Math.floor(Math.random()* (maxnumValue+1));
+		if (value != 0) {
             numbers.push(value);
         }
+        let occ= occurrences(numbers,value);
+        numbersDesc.push(occ);
 	}
 	Draw();
 }
@@ -28,8 +43,8 @@ function Draw(){
 	console.log(barWidth);
 	
 	for(let i=0; i< numbers.length; i++){
-		let line= '<div class="Barline" style="height: '+numbers[i]/2.4+'vh; width: '+barWidth+'px;"></div>';
-			document.getElementById("Graphic-container").innerHTML += line;
+		let line= '<div id="'+numbers[i]+':'+numbersDesc[i]+'" class="Barline" style="height: '+numbers[i]/3+'vh; width: '+barWidth+'px;"></div>';
+		document.getElementById("Graphic-container").innerHTML += line;
 	}
 }
 
@@ -38,5 +53,25 @@ function ChangeSortAlgorithm(name){
 }
 
 function Sort(){
-	alert(sortingalg);
+	if(sortingalg=="Insertion"){
+		InsertionSorting();
+	}
+	else if(sortingalg=="Selection"){
+		SelectionSorting();
+	}
+	else if(sortingalg=="Bubble"){
+		BubbleSorting();
+	}
+}
+
+async function InsertionSorting(){
+    alert("Insertion function");
+}
+
+async function SelectionSorting(){
+    alert("Selection function");
+}
+
+async function BubbleSorting(){
+    alert("Bubble function");
 }
